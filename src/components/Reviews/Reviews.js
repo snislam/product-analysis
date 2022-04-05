d5b6cfae8bba0917c9ff8f16f0eac54bf00f46af
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-04-05 20:42:15 +0600

    All done except readme

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -6,7 +6,7 @@ const Reviews = () => {
     return (
         <div className='mx-20 my-10'>
             <div className='text-center py-6'>
-                <h2 className='text-4xl text-center font-semibold'>Trusted By the Largest Product</h2>
+                <h2 className='text-4xl text-center font-semibold'>Trusted By the Huge Reviews</h2>
                 <p className='text-md my-3'>Product withmillion of users were made better with this car.</p>
             </div>
             <div className='grid grid-cols-3 gap-10'>
