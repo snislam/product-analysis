5ceba58f563ced9319401b4c7ec15c835ba5c725
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-04-05 19:47:00 +0600

    Not found and chart done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('chartdata.json')
+            .then(res => res.json())
+            .then(data => setData(data));
+    }, [])
     return (
-        <div>
-            <h2>Dashboard</h2>
-        </div>
+        <div className='mx-20 my-10 w-75 flex justify-between items-center'>
+            <div>
+                <h3 className='my-5 text-2xl font-semibold'>Sell per Month</h3>
+                <LineChart width={500} height={250} data={data}>
+                    <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
+            </div>
+            <div>
+                <h3 className='my-5 text-2xl font-semibold'>Investment vs Revenue per month</h3>
+                <BarChart width={500} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </div>
+        </div >
     );
 };
 
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 - Not found</h2>
+        <div className='flex flex-col justify-center items-center w-50 h-screen'>
+            <h1 className='text-6xl font-bold'>404 - Not found</h1>
+            <h3 className='text-md font-medium'>Opps! The url you enter is not correct. Please enter correct url or click on navbar.</h3>
         </div>
     );
 };
